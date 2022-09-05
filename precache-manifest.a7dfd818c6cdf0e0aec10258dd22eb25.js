7f56dc6942a29c4ccbd04cedf2e6431d457a9d2e
Muhammad Waqar Ahmad jamil <waqarsdk33@gmail.com>
2022-09-04 23:53:40 -0700

    Updates

---
+++ precache-manifest.a7dfd818c6cdf0e0aec10258dd22eb25.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "436124daa13b8dadfe93",
+    "url": "/learn-react/static/css/main.6a284cf3.chunk.css"
+  },
+  {
+    "revision": "436124daa13b8dadfe93",
+    "url": "/learn-react/static/js/main.436124da.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/learn-react/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "946d8365c0317c9ba6aa",
+    "url": "/learn-react/static/js/2.946d8365.chunk.js"
+  },
+  {
+    "revision": "f5c6c7fa21d3233ac3b9",
+    "url": "/learn-react/static/js/runtime~main.f5c6c7fa.js"
+  },
+  {
+    "revision": "18a8a7afc2729b927245d452d48df2f1",
+    "url": "/learn-react/index.html"
+  }
+];
